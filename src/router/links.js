1040e8b3b65f248b40f67615e6bec6b62a2c16c9
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2024-03-31 10:03:55 -0600

    se crea la recuperacion de password automaticamente generando y enviando tokens por correo electronico

--- src/router/links.js
+++ src/router/links.js
@@ -5,6 +5,7 @@ const database = require('../database');
 const addDatabase = require('../router/addDatabase');
 const databaseM = require('../mongodb');
 const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
+const helpers=require('../lib/helpers.js');
 //const delateDatabase=require('delateDatabase');
 const nodemailer = require('nodemailer'); //this is for send emails 
 const crypto = require('crypto');
@@ -187,20 +188,150 @@ router.get('/restart-password', async (req, res) => {
 });
 
 router.post('/restart-password', async (req, res) => {
-    await restart_password_send_token();
-    req.flash('success', 'Se envio un token a tu correo electronico usalo para restablecer tu password 👁️');
-    res.redirect('/fud/confirm-restart-password');
+    const {email}=req.body; //get the email of the user 
+    const token = await create_token(); // create a token
+    const idUser=await get_id_user_for_email(email);
+
+    //we will watching if exist a user with this email 
+    if(idUser){
+        //we going to save the token in the database 
+        if(await save_token_database(idUser,token)){
+            //if we can save the token in the database, send the token for email 
+            await restart_password_send_token(email,token);
+            req.flash('success', 'Se envio un token a tu correo electronico usalo para restablecer tu password 👁️');
+            res.redirect('/fud/confirm-restart-password');
+        }else{
+            //if we no can save the token in the database, show a message of error 
+            req.flash('message', 'No pudimos enviar el token a tu cuenta, intente de nuevo 👉👈');
+            res.redirect('/fud/restart-password');
+        }
+    }else{
+        req.flash('message', 'No existe un usuario con este correo 🤨');
+        res.redirect('/fud/restart-password');
+    }
+
 });
+
+async function get_id_user_for_email(email){
+    var queryText = 'SELECT * FROM "Fud".users WHERE email = $1';
+    var values = [email];
+    const result = await database.query(queryText, values);
+    const data = result.rows;
+    return data[0].id;
+}
+
+async function save_token_database(idUser,token){
+    try{
+        // get the date and hours current
+        const now = new Date();
+
+        // calculate token expiration date and time (5 minutes in the future)
+        const expiryTime = new Date(now.getTime() + 5 * 60 * 1000); // 5 minutes in milliseconds
+
+        // Query to insert the token into the reset_tokens table
+        const queryText = `
+            INSERT INTO "Fud".tokens (user_id, token, created_at, expiry_time)
+            VALUES ($1, $2, $3, $4)
+        `;
+        const values = [idUser, token, now, expiryTime];
+
+        // save the the token
+        await database.query(queryText, values);
+        return true;
+    }catch(error){
+        console.log('error to save the token '+error)
+        return false;
+    }
+}
 
 router.get('/confirm-restart-password', async (req, res) => {
     res.render(companyName + '/web/restartPassword')
 });
 
 router.post('/confirm-restart-password', async (req, res) => {
-    await restart_password_send_token();
-    req.flash('success', 'Tu password fue actualizada con exito');
-    res.redirect('/fud/login');
+    const {password1,password2,token}=req.body;
+    //we will getting the id of the user with the token 
+    const idUser=await get_id_user_for_token(token);
+    
+    //we will watching if this token have a user assigned
+    if(idUser){
+        //we will watching if the password is equals 
+        if(compare_password(password1,password2)){
+            //we will encrypting the password
+            const newPassword=await helpers.encryptPassword(password1);
+
+            //we will updating the new password in the database 
+            if(await update_password_user(idUser,newPassword)){
+                await delete_token(token); //delete the token used for the user 
+                //if the password was update with success, redirect to user to the web of login
+                req.flash('success', 'Tu contraseña fue actualizada con exito ❤️');
+                res.redirect('/fud/login');
+            }else{
+                //if not can update the password show a message of error
+                req.flash('message', 'La contraseñas no pudo actualizarse intentelo de nuevo 😰');
+                res.redirect('/fud/confirm-restart-password');
+            }
+        }else{
+            req.flash('message', 'La contraseñas no coinsiden 🤨');
+            res.redirect('/fud/confirm-restart-password');
+        }
+    }else{
+        //if the token not have a user assigned show a message of error
+        req.flash('message', 'Este Token no se encuentra en la base de datos o ya caduco 👁️');
+        res.redirect('/fud/confirm-restart-password');
+    }
 });
+
+async function get_id_user_for_token(token){
+    try {
+        var queryText = 'SELECT * FROM "Fud".tokens WHERE token = $1';
+        var values = [token];
+        const result = await database.query(queryText, values);
+        const data = result.rows;
+        return data[0].user_id;
+    } catch (error) {
+        console.error('Error to delete the token of the database:', error);
+        return false;
+    }
+}
+
+async function update_password_user(idUser, newPassword) {
+    try {
+        const queryText = `
+            UPDATE "Fud".users
+            SET password = $1
+            WHERE id = $2
+        `;
+        const values = [newPassword, idUser];
+        await database.query(queryText, values);
+
+        return true;
+    } catch (error) {
+        console.error('Error when update the new password:', error);
+        return false;
+    }
+}
+
+async function delete_token(token) {
+    try {
+        const queryText = `
+            DELETE FROM "Fud".tokens
+            WHERE token = $1
+        `;
+        const values = [token];
+        await database.query(queryText, values);
+    } catch (error) {
+        console.error('Error to delete the token of the database:', error);
+    }
+}
+
+function compare_password(P1,P2){
+    if (P1==''){
+        return false;
+    }
+
+    return P1==P2;
+}
 
 // function for create a new token for restart password
 function create_token() {
@@ -215,9 +346,8 @@ function create_token() {
     });
 }
 
-async function restart_password_send_token() {
-    const token = await create_token(); // create a token
-    const toEmail = 'fud-technology@hotmail.com'
+async function restart_password_send_token(email,token) {
+    const toEmail = 'fud-technology@hotmail.com' //email
     const subjectEmail = 'Restablecimiento de Password'
     const message = `
         <html>
